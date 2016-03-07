75327981123b40668fcf35c78f91d514de2a865b
Qian Zhang <qian.zhang@utdallas.edu>
2016-03-07 16:55:15 -0600

    Initial Commit

---
+++ hash_projects/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hash_projects
+//
+//  Created by yzc on 3/7/16.
+//  Copyright © 2016 yzc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
